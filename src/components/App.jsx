009099264af4eed95767a4aefccd2243f1994588
joe mckinney <mckinney.a.joe@gmail.com>
2018-07-01 12:16:26 -0700

    add stateful components to project to add dynamic ticket creation and adding to our ticketlist.  it will also add new rules to eslint file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,39 @@
 import React from 'react'
 import TicketList from './TicketList'
 import Header from './Header'
-import NewTicketForm from './NewTicketForm'
+import NewTicketControl from './NewTicketControl'
 import { Switch, Route } from 'react-router-dom'
 import Error404 from './Error404'
 
-function App(){
+class App extends React.Component {
 
-  return (
-    <div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterTicketList: []
+    };
+    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+  }
 
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={TicketList} />
-        <Route path='/newticket' component={NewTicketForm} />
-        <Route component={Error404} />
-      </Switch>
-    </div>
-  );
+  handleAddingNewTicketToList(newTicket) {
+    var newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.push(newTicket);
+    this.setState({masterTicketList: newMasterTicketList});
+  }
+
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
+          <Route path='/newticket' render={()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList}/>}/>
+          <Route component={Error404} />
+        </Switch>
+      </div>
+    );
+  }
+
 }
 
-export default App
+export default App;
